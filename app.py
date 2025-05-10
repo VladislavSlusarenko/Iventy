cc1b8b43d1cc171be5501b664c2ba0df1b416d64
Vlad Slusarenko <slusarenkov031@gmail.com>
2025-05-10 23:59:44 +0300

    Add Flask app with basic event scheduling

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    schedules = {
+        'Каріна': [
+            {'event_id': 'Подія 1', 'start': '10:00', 'location': 'Місце 1'},
+            {'event_id': 'Подія 2', 'start': '12:00', 'location': 'Місце 2'}
+        ]
+    }
+    return render_template('index.html', schedules=schedules)
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5001)
